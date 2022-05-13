5ccd588689d74fb9bb9a47a83adf6a4c4052276f
Iago Sant'anna <44707296+santannaiago@users.noreply.github.com>
2022-05-13 16:47:48 +0000

    Agora ele dá a largura de todas as colunas

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let container = 900
 let col = 12
 let gutter = 20
-let col_num = 12
+let col_num = 1
 
 let col_width = container / col - gutter
-let col_width_total = container / col * col_num
+let col_width_total = (container, col, col_num) => {return container / col * col_num}
 
 console.log("Largura de cada coluna: " + col_width + "px")
-if (col_num > 1) {
-  console.log(`Largura de ${col_num} colunas: ${col_width_total}px`)
-} else if (col_num == 1) {
-  console.log(`Largura de ${col_num} coluna: ${col_width_total}px`)
-} else {
-  console.log("Digite um número válido de colunas.")
+
+while(col_num <= col) {
+  if (col_num > 1) {
+    console.log(`Largura de ${col_num} colunas: ${col_width_total(container, col, col_num)}px`)
+  } else if (col_num == 1) {
+    console.log(`Largura de ${col_num} coluna: ${col_width_total(container, col, col_num)}px`)
+  }
+  col_num++
 }
